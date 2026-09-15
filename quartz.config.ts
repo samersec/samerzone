fdd579ae22272c28e2c5786f9e997787325911dc
samersec <soltanisamer02@gmail.com>
2026-09-15 22:05:41 +0100

    Fix GitHub Pages site URL

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "samersoltanii.github.io/samerzone",
+    baseUrl: "samersec.github.io/samerzone",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
